1d99077fc8eb771c2a7f776eda11328d5eadba81
Haruna Njie <harunanjie@hotmail.com>
2025-03-03 08:37:57 -0500

    move profile actions into actions filder

--- app/create-profile/_components/ProfileCreationForm.tsx
+++ app/create-profile/_components/ProfileCreationForm.tsx
@@ -10,7 +10,7 @@ import { toast } from 'sonner'
 import { 
   createCandidateProfileAction, 
   createEmployerProfileAction 
-} from '../_actions'
+} from '../../actions/profile'
 
 // Step Components
 import { BasicProfileForm } from './BasicProfileForm'
