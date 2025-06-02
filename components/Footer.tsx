bca0ba94f3f9b3cfd849b496fbd44fe533f71587
Haruna Njie <harunanjie@hotmail.com>
2025-06-02 03:06:00 -0400

    fix: update hover color for footer links to improve visual consistency

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,8 +14,8 @@ export default function Footer() {
           <div>
             <h4 className="text-lg  font-medium mb-3">For Job Seekers</h4>
             <ul className="space-y-2">
-              <li><Link href="/jobs" className="text-theme-gray-dark hover:text-theme-light">Find Jobs</Link></li>
-              <li><Link href="/sign-up" className="text-theme-gray-dark hover:text-theme-light">Create Profile</Link></li>
+              <li><Link href="/jobs" className="text-theme-gray-dark hover:text-theme-dark">Find Jobs</Link></li>
+              <li><Link href="/sign-up" className="text-theme-gray-dark hover:text-theme-dark">Create Profile</Link></li>
               {/* <li><Link href="/job-alerts" className="text-theme-gray-dark hover:text-theme-light">Job Alerts</Link></li> */}
             </ul>
           </div>
@@ -23,8 +23,8 @@ export default function Footer() {
           <div>
             <h4 className="text-lg  font-medium mb-3 ">For Employers</h4>
             <ul className="space-y-2">
-              <li><Link href="/employer/jobs/new" className="text-theme-gray-dark hover:text-theme-light">Post a Job</Link></li>
-              <li><Link href="/pricing" className="text-theme-gray-dark hover:text-theme-light">Pricing</Link></li>
+              <li><Link href="/employer/jobs/new" className="text-theme-gray-dark hover:text-theme-dark">Post a Job</Link></li>
+              <li><Link href="/pricing" className="text-theme-gray-dark hover:text-theme-dark">Pricing</Link></li>
               {/* <li><Link href="/resources" className="text-theme-gray-dark hover:text-theme-light">Resources</Link></li> */}
             </ul>
           </div>
@@ -33,8 +33,8 @@ export default function Footer() {
             <h4 className="text-lg  font-medium mb-3">Contact</h4>
             <ul className="space-y-2">
               {/* <li><Link href="/about" className="text-theme-gray-dark hover:text-theme-light">About Us</Link></li> */}
-              <li><Link href="/contact-us" className="text-theme-gray-dark hover:text-theme-light">Support</Link></li>
-              <li><Link href="/privacy" className="text-theme-gray-dark hover:text-theme-light">Privacy Policy</Link></li>
+              <li><Link href="/contact-us" className="text-theme-gray-dark hover:text-theme-dark">Support</Link></li>
+              <li><Link href="/privacy" className="text-theme-gray-dark hover:text-theme-dark">Privacy Policy</Link></li>
             </ul>
           </div>
         </div>
