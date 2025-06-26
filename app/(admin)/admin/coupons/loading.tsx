7d8b5150b0e175e423d3e1422f93fc171bf596a4
Haruna Njie <harunanjie@hotmail.com>
2025-06-26 06:32:50 -0400

    refactor: replace inline coupon details rendering with a dedicated CouponDetailsClient component for improved code organization and readability

---
+++ app/(admin)/admin/coupons/loading.tsx
@@ -0,0 +1,9 @@
+import RingLoaderSpinner from "@/components/loaders/ring-loader";
+
+export default function Loading() {
+  return (
+    <div className='flex justify-center items-center h-screen container mx-auto'>
+    <RingLoaderSpinner />
+  </div>
+  );
+} 
