1ca856edcf9c205adfd8a7966a82a42bdb304840
Haruna Njie <harunanjie@hotmail.com>
2025-08-05 07:04:51 -0400

    Refactor Remotion configuration and add entry point
    - Removed Tailwind CSS integration from Remotion configuration.
    - Added a new entry point file (src/index.ts) to register the Remotion root component.

--- marketing-video/remotion.config.ts
+++ marketing-video/remotion.config.ts
@@ -1,13 +1,7 @@
 import {Config} from '@remotion/cli/config';
-import {enableTailwind} from '@remotion/tailwind';
 
 Config.setVideoImageFormat('jpeg');
 Config.setOverwriteOutput(true);
 Config.setPixelFormat('yuv420p');
 Config.setCodec('h264');
-
-enableTailwind({
-	tailwindCssPath: './src/style.css',
-});
-
 Config.setStudioPort(3001);
